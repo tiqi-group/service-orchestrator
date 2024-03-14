1839f33b1eecae52bde947a29996be31f147a6ff
Mose Müller <mosemueller@gmail.com>
2024-03-14 16:24:22 +0100

    adds RefreshControl component

---
+++ frontend/src/components/RefreshControl.tsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useRef, useState } from "react";
+import {
+  ButtonGroup,
+  Tooltip,
+  Button,
+  Popper,
+  Grow,
+  Paper,
+  ClickAwayListener,
+  MenuList,
+  MenuItem,
+} from "@mui/material";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { runMethod, setAttribute } from "../utils/socket";
+
+const options: Record<string, number | null> = {
+  Off: null,
+  "1s": 1,
+  "2s": 2,
+  "5s": 5,
+  "10s": 10,
+  "30s": 30,
+  "1m": 60,
+};
+
+const getKeyByValue = (value: number | null) => {
+  return Object.keys(options).find((key) => options[key] === value);
+};
+
+type OptionKeys = keyof typeof options;
+type RefreshControlProps = {
+  refreshInterval: number | null;
+};
+
+export const RefreshControl = React.memo((props: RefreshControlProps) => {
+  const { refreshInterval } = props;
+  const [selectedKey, setSelectedKey] = useState<OptionKeys | undefined>(undefined);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const anchorRef = useRef<HTMLDivElement>(null);
+
+  const refreshDashboard = () => {
+    runMethod("update", "", {});
+  };
+
+  const handleToggle = () => {
+    setMenuOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: Event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target as HTMLElement)) {
+      return;
+    }
+    setMenuOpen(false);
+  };
+
+  const handleMenuItemClick = (key: OptionKeys) => {
+    setSelectedKey(key);
+    setMenuOpen(false);
+    setAttribute("update_wait_time", "", options[key]);
+    runMethod("stop_update_hosts", "", {});
+    runMethod("start_update_hosts", "", {});
+  };
+
+  useEffect(() => {
+    setSelectedKey(() => getKeyByValue(refreshInterval));
+  }, [props]);
+  return (
+    <>
+      <ButtonGroup
+        variant="contained"
+        ref={anchorRef}
+        aria-label="split button"
+        color="inherit">
+        <Tooltip title="Refresh Dashboard">
+          <Button
+            size="small"
+            aria-label="restart"
+            color="inherit"
+            onClick={refreshDashboard}>
+            <RestartAltIcon />
+          </Button>
+        </Tooltip>
+        <Tooltip title="Set auto refresh interval">
+          <Button
+            size="small"
+            onClick={handleToggle}
+            endIcon={<ArrowDropDownIcon />}
+            style={{ textTransform: "initial" }}>
+            {refreshInterval !== null && `${refreshInterval}s`}
+          </Button>
+        </Tooltip>
+      </ButtonGroup>
+
+      {/* Copied from https://mui.com/material-ui/react-button-group/#split-button */}
+      <Popper open={menuOpen} anchorEl={anchorRef.current} transition>
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin: placement === "bottom" ? "center top" : "center bottom",
+            }}>
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu">
+                  {Object.keys(options).map((key) => (
+                    <MenuItem
+                      key={key}
+                      selected={key === selectedKey}
+                      onClick={() => handleMenuItemClick(key)}>
+                      {key}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </>
+  );
+});
+
+RefreshControl.displayName = "RefreshControl";
